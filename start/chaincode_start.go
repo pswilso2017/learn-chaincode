28ad8d4b9fc4dd3669c9d1ddb2bb975bfe526b7c
pswilso <pswilso@us.ibm.com>
2017-03-23 09:51:57 -0500

    My changes and understanding git stuff

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -27,9 +27,6 @@ import (
 type SimpleChaincode struct {
 }
 
-// ============================================================================================================================
-// Main
-// ============================================================================================================================
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
@@ -51,7 +48,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	return nil, nil
 }
 
-// Invoke isur entry point to invoke a chaincode function
+// Invoke is our entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
